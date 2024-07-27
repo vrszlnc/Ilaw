26e80b409f9e3dcd110a69f985fb5253501b0b8f
zyvrex1 <vrex11032003@gmail.com>
2024-07-26 19:23:01 -0700

    update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonCardHeader, IonCardTitle, IonCardContent, IonCard } from '@ionic/angular/standalone';
+
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,10 +8,13 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  updateColor(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const colorPicker = input.value;
+    const colorDisplay = document.getElementById('colorDisplay') as HTMLDivElement;
+    colorDisplay.style.backgroundColor = colorPicker;
+  }
 }
